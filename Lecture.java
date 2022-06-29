2da944fa6ec589a8c633f9936c5285633d37f762
DarkNight <farhadgunners@gmail.com>
2022-06-29 21:12:57 +0200

    implementation, strings

---
+++ Lecture.java
@@ -0,0 +1,76 @@
+/*
+* ------------- 499 B B.Lecture -------------- 
+You have a new professor of graph theory and he speaks very quickly.
+You come up with the following plan to keep up with his lecture and make notes.
+
+You know two languages, and the professor is giving the lecture in the first one.
+The words in both languages consist of lowercase English characters, each language consists of several words.
+For each language, all words are distinct, i.e. they are spelled differently.
+Moreover, the words of these languages have a one-to-one correspondence, that is, for each word in each language,
+there exists exactly one word in the other language having has the same meaning.
+
+You can write down every word the professor says in either the first language or the second language.
+Of course, during the lecture you write down each word in the language in which the word is shorter.
+In case of equal lengths of the corresponding words you prefer the word of the first language.
+
+You are given the text of the lecture the professor is going to read.
+Find out how the lecture will be recorded in your notes.
+
+Input
+The first line contains two integers, n and m (1 ≤ n ≤ 3000, 1 ≤ m ≤ 3000) — the number of words in the professor's lecture and the number of words in each of these languages.
+
+The following m lines contain the words. The i-th line contains two strings a(i), b(i) meaning that the word ai belongs to the first language,
+the word bi belongs to the second language, and these two words have the same meaning.
+It is guaranteed that no word occurs in both languages, and each word occurs in its language exactly once.
+
+The next line contains n space-separated strings c1, c2, ..., cn — the text of the lecture.
+It is guaranteed that each of the strings ci belongs to the set of strings {a1, a2, ... am}.
+
+All the strings in the input are non-empty, each consisting of no more than 10 lowercase English letters.
+
+Output
+Output exactly n words: how you will record the lecture in your notebook. Output the words of the lecture in the same order as in the input.
+
+Input:
+4 3
+codeforces codesecrof
+contest round
+letter message
+codeforces contest letter contest
+
+Output:
+codeforces round letter round
+
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Lecture {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int words = input.nextInt();
+        int lines = input.nextInt();
+        Map<String, String> languages = new HashMap<>();
+        for (int i = 0; i < lines; i++) {
+            languages.put(input.next(), input.next());
+        }
+        String[] arr = new String[words];
+        for (int i = 0; i < words; i++) {
+            String temp = input.next();
+            String lang2word = languages.get(temp);
+            if(temp.length() > lang2word.length()){
+                arr[i] = lang2word;
+            }else{
+                arr[i] = temp;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        input.close();
+    }
+}
