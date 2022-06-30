ce801a2e2333286b65a63f1a791297c3eea02c7a
DarkNight <farhadgunners@gmail.com>
2022-06-30 17:53:07 +0200

    greedy, math

---
+++ Mislove.java
@@ -0,0 +1,64 @@
+/**
+Mislove had an array a1, a2, ⋯, an of n positive integers, but he has lost it.
+He only remembers the following facts about it:
+
+    * The number of different numbers in the array is not less than l and is not greater than r;
+    * For each array's element ai either a(i) = 1 or a(i) is even and there is a number a(i) / 2 in the array.
+
+For example, if n=5, l=2, r=3 then an array could be [1,2,2,4,4] or [1,1,1,1,2];
+but it couldn't be [1,2,2,4,8] because this array contains 4 different numbers;
+it couldn't be [1,2,2,3,3] because 3 is odd and isn't equal to 1;
+and it couldn't be [1,1,2,2,16] because there is a number 16 in the array but there isn't a number 16 / 2 = 8.
+
+According to these facts, he is asking you to count the minimal and the maximal possible sums of all elements in an array.
+
+Input
+The only input line contains three integers n, l and r (1 ≤ n ≤ 1000, 1 ≤ l ≤ r ≤ min(n,20)) — an array's size,
+the minimal number and the maximal number of distinct elements in an array.
+
+Output
+Output two numbers — the minimal and the maximal possible sums of all elements in an array.
+
+Input:
+4 2 2
+
+Output:
+5 7
+*/
+
+import java.util.Scanner;
+
+public class Mislove {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sze = input.nextInt();
+        int lowerBound = input.nextInt();
+        int upperBound = input.nextInt();
+        int uniq = 1;
+        int pow2 = 2;
+        int minSum = 0;
+        int maxSum = 0;
+        for (int i = 1; i < sze; i++) {
+            if(uniq < lowerBound){
+                minSum += pow2; 
+                uniq++;
+                pow2 *= 2;
+            }else{
+                minSum += 1;
+            }
+        }
+        int uniques = 1;
+        int pow = 2;
+        for (int i = 1; i < sze; i++) {
+            if(uniques < upperBound){
+                maxSum += pow;
+                uniques++;
+                pow *= 2;
+            }else{
+                maxSum += (pow/2);
+            }
+        }
+        System.out.println((minSum + 1) + " " + (maxSum + 1));
+        input.close();
+    }
+}
